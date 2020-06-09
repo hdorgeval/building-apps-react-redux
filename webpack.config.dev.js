c2e2d78e675d24b880aad7920fdc1fd252367139
hdorgeval <henri.dorgeval@outlook.fr>
2020-06-10 00:11:10 +0200

    feat(webpack): add configuration file

---
+++ webpack.config.dev.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  devTool: 'cheap-module-source-map',
+  entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+    devServer: {
+      stats: 'minimal',
+      overlay: true,
+      historyApiFallback: true,
+      disableHostCheck: true,
+      headers: {
+        'Acces-Control-Allow-Origin': '*',
+      },
+      https: false,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        favicon: 'src/favicon.ico',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /(\.css)$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  },
+};
