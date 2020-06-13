213a027f7d7cd3df7a2f126b4ac9519972443910
hdorgeval <henri.dorgeval@outlook.fr>
2020-06-13 21:22:34 +0200

    feat(app): introduce add course form

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Courses = () => {
-  return <h2>Courses</h2>;
+  const [title, setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`You have added '${title}' course!`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add Course</h3>
+      <input type="text" onChange={handleChange} value={title} />
+      <input type="submit" value="Save" />
+    </form>
+  );
 };
