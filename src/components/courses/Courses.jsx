951d397eebffe17185ed0aff189e50e1d1058e29
hdorgeval <henri.dorgeval@outlook.fr>
2020-07-15 13:40:00 +0200

    fix(courses): be able to navigate to new course page

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { CoursesList } from './CoursesList.jsx';
 
 export const Component = () => {
   return (
     <>
       <h2>Courses</h2>
-      <button style={{ marginBottom: 20 }} className="btn btn-primary add-course">
+      <Link to="/course" className="btn btn-primary add-course" style={{ marginBottom: 20 }}>
         Add course
-      </button>
+      </Link>
       <CoursesList />
     </>
   );
